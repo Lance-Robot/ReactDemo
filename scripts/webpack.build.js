a17352689dc64633049a7ee2541ca7e766c4f2d7
刘文俊 <fanatic_l@163.com>
2020-03-01 16:40:00 +0800

    1.完善VSCode开发环境，安装一些必要插件包括不限于JSHint，ESLint,Path Intellisense,Prettier 2.根据JSHint提示，修复代码格式问题和兼容问题 3.W104 -语法只能在ES6用，可在头部加注释/*jshint esversion: 6 */ 解决

--- scripts/webpack.build.js
+++ scripts/webpack.build.js
@@ -1,55 +1,56 @@
-const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-module.exports={
-    entry:{
-        index:'./src/index.js'
+/*jshint esversion: 6 *///解决jshint 提示w104的警告，使用ECMAScript 6特定语法
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    entry: {
+        index: './src/index.js'
     },
-    output:{
-        path:path.resolve(process.cwd(),"dist"),
-        filename:"js/[name].[chunkHash:5].js"
+    output: {
+        path: path.resolve(process.cwd(), "dist"),
+        filename: "js/[name].[chunkHash:5].js"
     },
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'postcss-loader',
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
                 ],
-          },
-          {
-            test: /\.less$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'postcss-loader',
-                {
-                    loader:"less-loader",
-                    options:{
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: "less-loader",
+                        options: {
 
-                    }
-                },
+                        }
+                    },
                 ],
-          },
+            },
         ],
-     },
+    },
 
     plugins: [
         new HtmlWebpackPlugin({
-            title:'webpack',
-            template:"public/index.html"
+            title: 'webpack',
+            template: "public/index.html"
         }),
         new MiniCssExtractPlugin({
-            filename:'css/[name].[chunkHash:5].css'
+            filename: 'css/[name].[chunkHash:5].css'
         }),
         
-      ],
-    devServer:{
-        port:3000,
-        open:true
+    ],
+    devServer: {
+        port: 3000,
+        open: true
 
-     }
+    }
     
-}
+};
