9fabbe4d79429a2a0443a4c361a705ac465e48b8
刘文俊 <fanatic_l@163.com>
2020-03-02 21:15:01 +0800

    1.file-loader,url-loader处理图片资源文件 2.url-loader 包含了file-loader,所以不依赖file-loader，file-loader 的options直接用于url-loader时就可以实现并存的效果

--- scripts/webpack.build.js
+++ scripts/webpack.build.js
@@ -3,54 +3,72 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    entry: {
-        index: './src/index.js'
-    },
-    output: {
-        path: path.resolve(process.cwd(), "dist"),
-        filename: "js/[name].[chunkHash:5].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                ],
-            },
-            {
-                test: /\.less$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    {
-                        loader: "less-loader",
-                        options: {
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(process.cwd(), "dist"),
+    filename: "js/[name].[chunkHash:5].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "less-loader",
+            options: {}
+          }
+        ]
+      },
+      //   {
+      //     test: /\.(png|jpe?g|gif)$/i,
+      //     use: [
+      //       {
+      //         loader: "file-loader",
+      //         options: {
+      //           name: "[name].[ext]",
+      //           outputPath: "images",
+      //           publicPath: "/images"
+      //         }
+      //       }
+      //     ]
+      //   },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+                limit: 800000,
+                name: "[name].[ext]",
+                outputPath: "images",
+                publicPath: "/images",
+            }
+          }
+        ]
+      }
+    ]
+  },
 
-                        }
-                    },
-                ],
-            },
-        ],
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'webpack',
-            template: "public/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[chunkHash:5].css'
-        }),
-        
-    ],
-    devServer: {
-        port: 3000,
-        open: true
-
-    }
-    
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack",
+      template: "public/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[chunkHash:5].css"
+    })
+  ],
+  devServer: {
+    port: 3000,
+    open: true
+  }
 };
