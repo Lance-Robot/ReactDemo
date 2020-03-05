304b3f81e21d4947d8b466598605a30250315ff0
刘文俊 <fanatic_l@163.com>
2020-03-05 22:14:04 +0800

    清除webpack内容

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/*jshint esversion: 6 */ //解决jshint 提示w104的警告，使用ECMAScript 6特定语法
-import less from './index.less';
-import css from './app.css';
-import { add } from './test';
-add(1,2);
-console.log('This is Homepage');
--- src/test.js
+++
@@ -1,5 +0,0 @@
-/*jshint esversion: 6 */ //解决jshint 提示w104的警告，使用ECMAScript 6特定语法
-export const add = (x, y) => {
-    //console.log(`${"中午跟\n456"}`);
-    console.log(`${x} + ${y} = ${x + y}`);
-};
