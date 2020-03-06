f0c6fd14693c43db60d6ad6f875da435889f6c2e
刘文俊 <fanatic_l@163.com>
2020-03-06 16:15:07 +0800

    1.类方式创建React组件 2.jsx的原理，封装了一层语法糖 3.React.createElement(type,[props],[...children]) 创建元素转化为js原生语法

--- ProjectTodo/src/index.js
+++ ProjectTodo/src/index.js
@@ -1,18 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render} from 'react-dom';
 
-//函数式组件，函数名要大写，jsx里插入js的代码需要加一层大括号，包括注释
-const  App = (props) =>{
-        return  (
-            <div>
-                <h1 title = {props.title}>Welcome React{props.title}</h1>
-                <p>最好的React{props.title}</p>
-            </div>
+// //函数式组件，函数名要大写，jsx里插入js的代码需要加一层大括号，包括注释
+// const  App = (props) =>{
+//         return  (
+//             <div>
+//                 <h1 title = {props.title}>Welcome React{props.title}</h1>
+//                 <p>最好的React{props.title}</p>
+//             </div>
+//         )
+// };
+
+
+// ReactDOM.render(
+//     <App title = '1901'/>,
+//     document.querySelector('#root')
+// )
+
+//类组件 jsx原理  React.createElement(type,[props],[...children])
+class App extends React.Component{
+    render(){
+        return(
+            React.createElement(
+                'div',
+                {
+                    className : 'app',
+                    id : 'root'
+                },
+                React.createElement(
+                    'h1',
+                    {
+                        className :'title'
+                    },
+                    'jsx原理'
+                ),
+                React.createElement(
+                    'p',
+                    null,
+                    '子内容'
+                )
+            )
+
         )
-};
+        
+         
+    }
+}
 
 
-ReactDOM.render(
-    <App title = '1901'/>,
-    document.querySelector('#root')
-)
+render(
+     <App desc = "1901"/>,
+    document.querySelector("#root")
+);
