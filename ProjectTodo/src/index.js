156836bbaa080fae8f21ad927c80d805baaab84c
刘文俊 <fanatic_l@163.com>
2020-03-06 19:50:25 +0800

    1.classNames 库的使用 2.styled-components库的使用

--- ProjectTodo/src/index.js
+++ ProjectTodo/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render} from 'react-dom';
+import './index.css'
+import classNames  from 'classnames';
+import styled from 'styled-components';
 
 // //函数式组件，函数名要大写，jsx里插入js的代码需要加一层大括号，包括注释
 // const  App = (props) =>{
@@ -16,30 +19,25 @@ import { render} from 'react-dom';
 //     <App title = '1901'/>,
 //     document.querySelector('#root')
 // )
-
+const Title =styled.h1`
+    color : #F00
+`
 //类组件 jsx原理  React.createElement(type,[props],[...children])
 class App extends React.Component{
+    
     render(){
+
+        const  style = {color :'#F00'};
+
         return(
-            React.createElement(
-                'div',
-                {
-                    className : 'app',
-                    id : 'root'
-                },
-                React.createElement(
-                    'h1',
-                    {
-                        className :'title'
-                    },
-                    'jsx原理'
-                ),
-                React.createElement(
-                    'p',
-                    null,
-                    '子内容'
-                )
-            )
+            <div>
+                <Title  >元素中的样式</Title>
+                <ol>
+                    <li style = {style}>使用内联创建</li>
+                    <li  className = {classNames('a',{'b':true,'c':false})}>使用class方式，但是在react里class要写成className</li>
+                </ol>
+            </div>
+           
 
         )
         
